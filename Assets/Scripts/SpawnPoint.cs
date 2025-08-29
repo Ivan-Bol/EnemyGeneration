75c0aa9e4250185e86e7ff2ea5b5c99553aca329
Ivan-Bol <bolschakoff.iwan2017@gmail.com>
2025-08-29 14:49:40 +0700

    Update SpawnPoint.cs

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -4,10 +4,9 @@ public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] private Enemy _enemyPrefab;
 
-    public void SpawnEnemy(Vector3 rotation)
+    public void SpawnEnemy(Vector3 direction)
     {
-        Enemy enemy = Instantiate(_enemyPrefab); ;
-        enemy.transform.position = transform.position;
-        enemy.transform.eulerAngles = rotation;
+        Enemy enemy = Instantiate(_enemyPrefab);
+        enemy.Initialize(direction, transform.position);
     }
 }
