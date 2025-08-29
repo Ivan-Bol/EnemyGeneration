7210c01de0a9607bdafa8845bc1141ac69a2c89b
Ivan-Bol <bolschakoff.iwan2017@gmail.com>
2025-08-29 14:49:05 +0700

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
+    private Vector3 _direction;
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * _movementSpeed * Time.deltaTime);
+        transform.Translate(_direction * _movementSpeed * Time.deltaTime);
     }
-}
+
+    public void Initialize(Vector3 direction, Vector3 startPosition)
+    {
+        transform.position = startPosition;
+        _direction = direction;
+    }
